3d6b81148fcbedf044593b14c8691e32c2d5c755
Ahabwe Emmanuel <ahabweemma@gmail.com>
2024-08-06 05:06:20 +0300

    updated the build workflow to build and run the app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,23 +7,9 @@ export class AppController {
 
   @Get('health')
   getHealth(@Req() req): any {
-    var ck = 'debunkbot12@!!';
-    let name = 'debunkbot'
     const originHost = req.headers.host;
 
-    const allowedHostname = 'sales.jubileeallianz.co.ug';
-
-    if (originHost === allowedHostname) {
-      return {
-        ck: ck,
-        nm: name,
-        h: originHost
-      };
-    } else {
-      return {
-        h: originHost
-      }
-    }
+    return 'health'
 
   }
 }
