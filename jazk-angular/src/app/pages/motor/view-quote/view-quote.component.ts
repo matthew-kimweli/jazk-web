c90cf374b89a7df8ae4aadb529028bd2da5de2c3
Ahabwe Emmanuel <ahabweemma@gmail.com>
2024-08-18 20:35:20 +0300

    updated quotation page

--- jazk-angular/src/app/pages/motor/view-quote/view-quote.component.ts
+++ jazk-angular/src/app/pages/motor/view-quote/view-quote.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../_components/header/header.component";
+import { HeaderComponent } from '../../_components/header/header.component';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-view-quote',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, HeaderComponent],
   templateUrl: './view-quote.component.html',
-  styleUrl: './view-quote.component.css'
+  styleUrl: './view-quote.component.css',
 })
 export class ViewQuoteComponent {
   vehicle = {
     makeModel: 'Toyota Corolla',
     year: 2019,
-    registration: 'KAA 123A'
+    registration: 'KAA 123A',
   };
-
 
   insurance = {
     coverageType: 'Comprehensive',
@@ -25,33 +25,34 @@ export class ViewQuoteComponent {
     limits: {
       thirdPartyPropertyDamage: 500000,
       bodilyInjuryPerPerson: 1000000,
-      bodilyInjuryPerAccident: 3000000
+      bodilyInjuryPerAccident: 3000000,
     },
     excesses: {
       ownDamage: 2.5,
       theft: 5.0,
-      windscreenDamage: 10.0
+      windscreenDamage: 10.0,
     },
     benefits: {
-      medicalExpenses: 100000
+      medicalExpenses: 100000,
     },
     enhancements: {
       windscreenCover: 20000,
-      lossOfUse: 2000
-    }
+      lossOfUse: 2000,
+    },
   };
+emailQuoteDetails: any = {};
 
-
-  constructor(private router: Router){
-
-  }
+  constructor(private router: Router) {}
 
   purchase() {
-    
-    this.router.navigate(['motor-kyc'])
+    this.router.navigate(['motor-kyc']);
   }
 
+  downloadQuote() {}
+  emailQuote() {}
+
   goBack() {
-    history.back()
+    history.back();
   }
+
 }
