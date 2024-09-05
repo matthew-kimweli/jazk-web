c0c29404db6691cedbbd443a47f81593b0fe87c9
Ahabwe Emmanuel <ahabweemma@gmail.com>
2024-09-05 07:15:41 +0300

    updated

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from "@nestjs/common";
 
 const nodemailer = require("nodemailer");
-let unirest = require("unirest");
+// let unirest = require("unirest");
+const axios = require('axios');
+
 
 @Injectable()
 export class AppService {
-
-
-    
   onModuleInit() {
     this.initCloudFunctions();
   }
@@ -52,40 +51,59 @@ export class AppService {
   }
 
   initCloudFunctions() {
-
     Parse.Cloud.define("paympesa", async (request) => {
       let params = request.params;
       let payload = params.payload;
 
       try {
-        let req = await unirest
-          .post("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest")
-          .headers({
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer nGNZ4jA4yX3am5EFuPaqmaeuszfo'
-          })
-          .send(JSON.stringify({
-            "BusinessShortCode": 174379,
-            "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjQwOTA1MDcwMTU0",
-            "Timestamp": Date.now(),
-            "TransactionType": "CustomerPayBillOnline",
-            "Amount": 1,
-            "PartyA": payload.phone,
-            "PartyB": 174379,
-            "PhoneNumber": payload.phone,//254708374149,
-            "CallBackURL": "https://jazk-web-fgefcwaabpdbchbr.northeurope-01.azurewebsites.net/receivepayment",
-            "AccountReference": "CompanyXLTD",
-            "TransactionDesc": "Payment of X" 
-          }))
-    
-        if (req.error) throw new Error(req.error);
-        console.log(req.raw_body);
-        return req.raw_body
+        const response = await axios.get(
+          "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+          {
+            headers: {
+              Authorization:
+                "Basic S3FUNDk2V1c1V09LMmxjT3AwdnRzQjZxVWFYaHl0UXhwbUdzS2FWS1kza0xNTzA4OlVyaU1lT0NQamVlMDNuaFo0SDZhTlZsNkU0ZWJ2TEExQWNWbnFnRnUxb08yZmJ3c0FkSU1vN2VTWEdXMmRERWM=",
+            },
+          }
+        );
+        
+// {
+//   "access_token": "xHM8FgkuvIujKU7ZoD89Q6xFGC7u",
+//   "expires_in": "3599"
+// }
+        console.log('access token', response.data);
+        let tokenData = response.data
+        if(tokenData && tokenData.access_token){
+
+          const response = await axios.post(
+            'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest',
+            {
+              "BusinessShortCode": 174379,
+              "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjQwOTA1MDcxMTQ1",
+              "Timestamp": "20240905071145",
+              "TransactionType": "CustomerPayBillOnline",
+              "Amount": 1,
+              "PartyA": 254708374149,
+              "PartyB": 174379,
+              "PhoneNumber": 254708374149,
+              "CallBackURL": "https://mydomain.com/path",
+              "AccountReference": "CompanyXLTD",
+              "TransactionDesc": "Payment of X"
+            },
+            {
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${tokenData.access_token}`,
+              },
+            }
+          );
+          console.log(response.data);
+          return response.data
+        }
+
       } catch (error) {
         console.error("Request failed:", error);
-        return String(error).toString()
+        return String(error).toString();
       }
-
     });
   }
 }
