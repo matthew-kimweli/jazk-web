5fd74fbb8f7afe7c824455d3f177c7d34f2454b6
Ahabwe Emmanuel <ahabweemma@gmail.com>
2024-09-10 10:23:26 +0300

    updated

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
   var serverURL = `http://127.0.0.1:${port}/parse`;
   // databaseUri = "mongodb+srv://admin:6iT4wLTN6rXYa8B@cluster0.2toly.mongodb.net/debunkbot?retryWrites=true&w=majority"
   // var serverURL = `https://jazk-web-fgefcwaabpdbchbr.northeurope-01.azurewebsites.net/parse`;
-  var publicServerURL = process.env.PUBLICSERVERURL || 'https://jazk-web-fgefcwaabpdbchbr.northeurope-01.azurewebsites.net/parse' 
+  var publicServerURL = process.env.PUBLICSERVERURL || serverURL
   
   // databaseUri = 'postgres://postgres:postgres@157.230.47.71:5432/jazke'
   databaseUri = 'postgres://jazkadmin:Developement.313*@jazk-postgres-fdb.postgres.database.azure.com/web_dev'
