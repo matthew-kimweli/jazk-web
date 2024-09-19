bc5d99990a8c9761f12d0cd2393de8a4a27ff182
Ahabwe Emmanuel <ahabweemma@gmail.com>
2024-09-19 11:23:17 +0300

    updated

--- jazk-angular/src/app/pages/portal-docs/policy-schedule/policy-schedule.component.ts
+++ jazk-angular/src/app/pages/portal-docs/policy-schedule/policy-schedule.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ParseService } from '../../../services/parse.service';
 
 @Component({
   selector: 'app-policy-schedule',
@@ -11,5 +13,48 @@ import { Component } from '@angular/core';
 export class PolicyScheduleComponent {
   autoMobile: any;
   motorClass: any = 'private';
+  sale: any;
+  today: string = '';
+
+  
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    public parseService: ParseService
+  ) {
+    this.getToday();
+  }
+
+  getToday() {
+    var date = new Date(); // create a new date object
+    var formattedDate = date.toISOString().substring(0, 10); // format the date as yyyy-mm-dd and remove the time and timezone
+    console.log(formattedDate); // output the formatted date
+    this.today = formattedDate;
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((parameter) => {
+      let id = parameter['id'];
+
+      if (id) {
+        this.fetchSale(id);
+      }
+    });
+  }
+
+  async fetchSale(id: any) {
+    try {
+      console.log('fetching sale', id);
+      this.parseService.fetching = true;
+      let query = new Parse.Query('JazkeSale');
+      let sale = await query.get(id);
+      console.log('sale', sale);
+      this.sale = sale;
+      
+      this.parseService.fetching = false;
+    } catch (error) {
+      console.error(error);
+      this.parseService.fetching = true;
+    }
+  }
 
 }
