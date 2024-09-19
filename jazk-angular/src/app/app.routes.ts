8cf4940d47c78d4a07d34033b7d4457321f59d50
Calvin Tinka <tinkacalvin@gmail.com>
2024-09-19 05:47:57 +0300

    updates

--- jazk-angular/src/app/app.routes.ts
+++ jazk-angular/src/app/app.routes.ts
@@ -17,7 +17,7 @@ import { QuotationSalesListComponent } from './pages/agent/quotation-sales-list/
 import { ViewQuoteSubdetailsComponent } from './pages/motor/view-quote-subdetails/view-quote-subdetails.component';
 import { GenerateReportComponent } from './pages/report_list/generate-report/generate-report.component';
 import { MotorPaymentSuccessComponent } from './pages/motor/motor-payment-success/motor-payment-success.component';
-import { ValuationLetterComponent } from './pages/portal-docs/valuation-letter/valuation-letter.component';
+import { PolicyScheduleComponent } from './pages/portal-docs/policy-schedule/policy-schedule.component';
 
 export const routes: Routes = [
   {
@@ -110,7 +110,7 @@ export const routes: Routes = [
 
   {
     path: 'motor-docs',
-    component: ValuationLetterComponent,
+    component: PolicyScheduleComponent,
   },
 
   
--- jazk-angular/src/app/pages/portal-docs/policy-schedule/policy-schedule.component.ts
+++ jazk-angular/src/app/pages/portal-docs/policy-schedule/policy-schedule.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-policy-schedule',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './policy-schedule.component.html',
   styleUrl: './policy-schedule.component.css'
 })
 export class PolicyScheduleComponent {
+  autoMobile: any;
+  motorClass: any = 'private';
 
 }
